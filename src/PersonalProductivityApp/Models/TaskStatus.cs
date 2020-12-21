29ddb5286058e97d955ce2dd9b8c50c1e5ab887f
Isaac Bailey <vanillaike@gmail.com>
2020-12-20 19:36:28 -0600

    Add and display dummy data

--- src/PersonalProductivityApp/Models/TaskStatus.cs
+++ src/PersonalProductivityApp/Models/TaskStatus.cs
@@ -1,15 +1,22 @@
 using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace PersonalProductivityApp.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum TaskStatus
     {
+        [EnumMember(Value = "Planning")]
         [Description("Planning")]
         Planning = 0,
+        [EnumMember(Value = "Todo")]
         [Description("Todo")]
         Todo = 1,
+        [EnumMember(Value = "InProgress")]
         [Description("In Progress")]
         InProgress = 2,
+        [EnumMember(Value = "Finished")]
         [Description("Finished")]
         Finished =3
     }
